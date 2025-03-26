76278d4108b319c3a0d8189086eeea78e408eb90
Rahat <rayhanulrahat8123@gmail.com>
2025-03-26 06:27:17 +0600

    Constant thing For The App

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,10 @@
 
+import 'package:agrocart/app.dart';
+import 'package:agrocart/main.dart';
 import 'package:agrocart/utils/theme/theme.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main(){
   runApp(App());
-}
-class App extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme,
-
-    );
-  }
-
 }
